e3ef1154d0e2a2002ffede716340f58535aadeab
SergeyFedos0 <feddoseev@gmail.com>
2025-03-06 07:45:55 +0200

    finished logic js part

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// this module is responsible for rendering gameboard
 const gameboard = (() => {
   let gameBoard = ['','','','','','','','',''];
 
@@ -15,14 +15,14 @@ const gameboard = (() => {
   }
   const update = (index, marker) => {
     
+    
     gameBoard[index] = marker;
     gameboard.render();
     
    
   }
-  const getGameboard = () => {
-    return gameBoard;
-  }
+  const getGameboard = () => gameBoard;
+  
 
 return {
   render,
@@ -31,19 +31,18 @@ return {
 }
 
 })();
-
+// this module is responsible for controlling game and logic
 const game = (() => {
+  let gameOver;
   let currentPlayerIndex;
   const players = [{
     name: document.querySelector('.player1-name').value,
     marker: 'X'
   }, { name: document.querySelector('.player2-name').value,
        marker: 'O'
-  }]
-
-
-
+  }];
   const start = () => {
+    gameOver = false;
     currentPlayerIndex = 0;
   gameboard.render();
   const squares = document.querySelectorAll('.square').forEach((square) => {
@@ -51,19 +50,69 @@ const game = (() => {
   })
 }
 const handleClick = (event) => {
+  if (gameOver) return;
   const targ = parseInt(event.target.id.split('-')[1]);
   if ( gameboard.getGameboard()[targ] !== '') {
     return;
   } 
+  
   gameboard.update(targ, players[currentPlayerIndex].marker)
+ 
   
    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0
-  
+   if ( checkForWin(gameboard.getGameboard())) {
+    const message = document.querySelector('.message').innerHTML = `${players[currentPlayerIndex].name} won!`
+    return;
+  } else if (game.checkForTie()) {
+    alert('tie!')
+
+  }
 }
+const restart = () => {
+  gameboard.getGameboard().forEach((square, index, arr) => {
+    arr[index] = '';
+  })
+    gameboard.render();
+  }
+  function checkForWin(arr) {
+    
+    const winCombos = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (let i = 0; i < winCombos.length; i++) {
+      const [a, b, c] = winCombos[i];
+      if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+        gameOver = true;
+        
+        return true;
+      }
+    }
+   
+    return false;
+  }
+  const checkForTie = () => {
+    for (let i = 0; i < gameboard.getGameboard().length; i++) {
+      if (gameboard.getGameboard()[i] === '') return false;
+    }
+    gameOver = true;
+     return true;
+    }
+   
 
 return {
   start,
-  handleClick
+  handleClick,
+  restart,
+  checkForWin,
+  checkForTie,
+  gameOver
  
 }
 
@@ -78,7 +127,8 @@ return {
 
 
 
-
+const restartBtn = document.querySelector('.reset-game');
+restartBtn.addEventListener('click', game.restart)
 
 const startBtn = document.querySelector('.start-game');
  startBtn.addEventListener('click', game.start)
